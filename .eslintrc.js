f16df06ddf7d13a98969c18f7dea855050731f63
roman <r.maorkhak@gmail.com>
2025-02-14 10:39:12 +0000

    added pre commit eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended'],
   parserOptions: {
     project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
     sourceType: 'module',
-    ecmaVersion: 2020,
-    ecmaFeatures: {
-      experimentalDecorators: true,
-    },
-  },
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'no-console': 'warn',
-    'prettier/prettier': 'error', //2323
   },
 };
